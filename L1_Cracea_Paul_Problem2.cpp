2a40b063f58bd36dd4aa990807612cdb50dae702
Paul <165474033+PaulMondialu@users.noreply.github.com>
2026-03-03 19:17:11 +0200

    Update L1_Cracea_Paul_Problem2.cpp

--- L1_Cracea_Paul_Problem2.cpp
+++ L1_Cracea_Paul_Problem2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int prim(int x) {
     for (int i = 2; i <= x / 2; i++) {
@@ -19,10 +20,49 @@ void PrimN(int n) {
 
 
 }
+
+int cmmdc(int a,int b) {
+
+
+    while(a != b) {
+        if(a > b) a = a - b;
+        else b = b - a;
+    }
+    return a;
+
+
+}
+void RelativPrim(std::vector<int> numere) {
+    int inc=0,inc2=0;
+    int fin=0,fin2=0;
+    int maxl=INT_MIN;
+    for (int i = 0; i <numere.size(); i++) {
+        if (cmmdc(numere[i],numere[i+1])==1 ) {
+            inc=i;
+            fin=i;
+            i++;
+            while (cmmdc(numere[i],numere[i+1])==1) {
+                fin=i;
+                i++;
+            }
+            if (fin-inc > maxl) {
+                maxl=fin-inc;
+                inc2=inc;
+                fin2=fin;
+            }
+        }
+    }
+    for (int i = inc2; i <=fin2; i++) {
+        std::cout << numere[i] << std::endl;
+    }
+
+}
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
 
-    PrimN(10);
+    //PrimN(100);
+    std::vector<int> lista={15, 28, 14, 21, 10, 33, 35, 12};
+    RelativPrim(lista);
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
